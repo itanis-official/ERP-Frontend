5e878ceb94e080c435b3c9c119c065b3f75612a4
itanis-official <support@itanis.tn>
2026-05-11 16:41:56 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -20,16 +20,3 @@ createRoot(document.getElementById('root')!).render(
     </AppThemeProvider>
   </ThemeModeProvider>,
 )
-import { BrowserRouter } from "react-router-dom"
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import App from "./App"
-import "./index.css"
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-)
